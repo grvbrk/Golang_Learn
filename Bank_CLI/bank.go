7ddfc49a26a482786525c10b911d6e1349366a7d
Sinnnnnn <gouravbarikofficial@gmail.com>
2024-04-04 17:15:45 +0530

    Logic finished

--- Bank_CLI/bank.go
+++ Bank_CLI/bank.go
@@ -4,28 +4,87 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strconv"
 )
 
 func main() {
 
 	var username string
 	var currOption int
+	loop := true
 
-	homePage()
-	fmt.Scan(&currOption)
+	for loop {
+		homePage()
+		fmt.Scan(&currOption)
 
-	if currOption == 1 {
-		fmt.Println("Your name?")
-		fmt.Scan(&username)
-		openNewAccount(username)
-	} else if currOption == 2 {
+		if currOption == 1 {
+			fmt.Println("Your name?")
+			fmt.Scan(&username)
+			openNewAccount(username)
+		} else if currOption == 2 {
+			if username == "" {
+				fmt.Println("Your name?")
+				fmt.Scan(&username)
+			}
 
-	} else if currOption == 3 {
+			account := isAccountExists(username)
+			if !account {
+				fmt.Println("Account does not exist. Do you want to create a new Account?")
+				userCreatesAnewAccount := false
+				fmt.Scan(&userCreatesAnewAccount)
+				if userCreatesAnewAccount {
+					openNewAccount(username)
+				}
+			}
 
-	} else if currOption == 4 {
+			balance := getBalance(username)
+			fmt.Printf("Your account balance is %f0.2", balance)
 
-	} else if currOption == 5 {
-		return
+		} else if currOption == 3 {
+			if username == "" {
+				fmt.Println("Your name?")
+				fmt.Scan(&username)
+			}
+
+			account := isAccountExists(username)
+			if !account {
+				fmt.Println("Account does not exist. Do you want to create a new Account?")
+				userCreatesAnewAccount := false
+				fmt.Scan(&userCreatesAnewAccount)
+				if userCreatesAnewAccount {
+					openNewAccount(username)
+				}
+			}
+
+			fmt.Println("Enter deposit amount: ")
+			var amount float64
+			fmt.Scan(&amount)
+			depositAmount(username, amount)
+
+		} else if currOption == 4 {
+			if username == "" {
+				fmt.Println("Your name?")
+				fmt.Scan(&username)
+			}
+
+			account := isAccountExists(username)
+			if !account {
+				fmt.Println("Account does not exist. Do you want to create a new Account?")
+				userCreatesAnewAccount := false
+				fmt.Scan(&userCreatesAnewAccount)
+				if userCreatesAnewAccount {
+					openNewAccount(username)
+				}
+			}
+
+			fmt.Println("Enter withdraw amount: ")
+			var amount float64
+			fmt.Scan(&amount)
+			withdrawAmount(username, amount)
+
+		} else if currOption == 5 {
+			loop = false
+		}
 	}
 }
 
@@ -39,6 +98,23 @@ func homePage() {
 }
 
 func openNewAccount(username string) {
+
+	account := isAccountExists(username)
+	if !account {
+		subfolder := "accounts"
+		filename := username + ".txt"
+		filePath := filepath.Join(subfolder, filename)
+
+		os.MkdirAll(subfolder, os.ModePerm)
+		os.WriteFile(filePath, []byte("0"), 0644)
+		fmt.Println("Your account has been created!")
+		return
+	}
+
+	fmt.Println("You already have an account")
+}
+
+func isAccountExists(username string) bool {
 	subfolder := "accounts"
 	filename := username + ".txt"
 	filePath := filepath.Join(subfolder, filename)
@@ -46,12 +122,44 @@ func openNewAccount(username string) {
 	_, err := os.Stat(filePath)
 	if err != nil {
 		if os.IsNotExist(err) {
-			os.MkdirAll(subfolder, os.ModePerm)
-			os.WriteFile(filePath, []byte("0"), 0644)
-			return
+			return false
 		}
 	}
+	return true
+}
 
-	fmt.Println("You already have an account")
+func getBalance(username string) float64 {
+	subfolder := "accounts"
+	filename := username + ".txt"
+	filePath := filepath.Join(subfolder, filename)
 
+	data, _ := os.ReadFile(filePath)
+
+	balance, _ := strconv.ParseFloat(string(data), 64)
+
+	return balance
+}
+
+func depositAmount(username string, deposit float64) {
+	currentBalance := getBalance(username)
+	updatedBalance := strconv.FormatFloat(currentBalance+deposit, 'f', -1, 64)
+
+	subfolder := "accounts"
+	filename := username + ".txt"
+	filePath := filepath.Join(subfolder, filename)
+	os.WriteFile(filePath, []byte(updatedBalance), 0644)
+
+	fmt.Printf("Success! Your new balance is: %v0.1 \n", updatedBalance)
+}
+
+func withdrawAmount(username string, withdraw float64) {
+	currentBalance := getBalance(username)
+	updatedBalance := strconv.FormatFloat(currentBalance-withdraw, 'f', -1, 64)
+
+	subfolder := "accounts"
+	filename := username + ".txt"
+	filePath := filepath.Join(subfolder, filename)
+	os.WriteFile(filePath, []byte(updatedBalance), 0644)
+
+	fmt.Printf("Success! Your new balance is: %v0.1 \n", updatedBalance)
 }
